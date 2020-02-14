4d8c7a0c1b9e5974e0b3db729e56aee31e792fae
Kit La Touche <kit@transneptune.net>
2020-02-14 11:48:29 -0700

    Backfill more tests

--- test/store.test.js
+++ test/store.test.js
@@ -1,4 +1,12 @@
-import { getAuthToken, setAuthToken, setEnableConfig } from '../src/lib/store';
+import {
+  getAuthToken,
+  getProjectId,
+  getTask,
+  setAuthToken,
+  setEnableConfig,
+  setProjectId,
+  setTask,
+} from '../src/lib/store';
 import trelloMock from './trello-mock.js';
 
 describe('setEnableConfig', () => {
@@ -35,5 +43,62 @@ describe('getAuthToken', () => {
       }),
     };
     expect(await getAuthToken(localTrelloMock)).toEqual(null);
+  });
+});
+
+describe('setProjectId', () => {
+  test('sets config on Trello', () => {
+    setProjectId(trelloMock, 'someId');
+
+    expect(trelloMock.set).toHaveBeenCalledWith(
+      'board',
+      'shared',
+      'harvestProjectId',
+      'someId',
+    );
+  });
+});
+
+describe('getProjectId', () => {
+  test('returns the projectIdStr', async () => {
+    const localTrelloMock = {
+      get: jest.fn().mockImplementation(() => '1'),
+    };
+    const actual = await getProjectId(localTrelloMock);
+
+    expect(actual).toEqual(1);
+  });
+});
+
+describe('setTask', () => {
+  test('calls set on Trello', () => {
+    setTask(trelloMock, { id: 1, name: 'test' });
+
+    expect(trelloMock.set).toHaveBeenCalledWith(
+      'card',
+      'shared',
+      'harvestTask',
+      '{"id":1,"name":"test"}',
+    );
+  });
+});
+
+describe('getTask', () => {
+  test('calls set on Trello', async () => {
+    const localTrelloMock = {
+      get: jest.fn().mockImplementation(() => '{"id":1,"name":"test"}'),
+    };
+    const actual = await getTask(localTrelloMock);
+
+    expect(actual).toEqual({ id: 1, name: 'test' });
+  });
+
+  test('defaults to null', async () => {
+    const localTrelloMock = {
+      get: jest.fn().mockImplementation(() => ''),
+    };
+    const actual = await getTask(localTrelloMock);
+
+    expect(actual).toEqual(null);
   });
 });
